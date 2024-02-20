98ed954efbbe63104d97519f5a6e627ea59e5b48
Justin <j.pietzsch@bbw-fi.de>
2024-02-20 09:06:40 +0100

    Speiseplan
    erstellt und stuff gemacht dies das

--- src/pages/Speiseplan.js
+++ src/pages/Speiseplan.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
 
 function Speiseplan() {
   return (
-    <div>speiseplan</div>
-  )
+    <div className=" bg-site-background container mx-auto px-4 text-white min-h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 py-8">
+        <div className="p-4 border border-gray-200 rounded-md hover:shadow-md">
+          <img
+            src="Placeholder.jpg"
+            alt="Placeholder"
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-4">
+            <h2 className="text-lg font-bold">Placeholder</h2>
+            <p className=" text-white">
+              Description of the Placeholder here.
+            </p>
+          </div>
+        </div>
+        <div className="p-4 border border-gray-200 rounded-md hover:shadow-md">
+          <img
+            src="Placeholder.jpg"
+            alt="Placeholder"
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-4">
+            <h2 className="text-lg font-bold">Placeholder</h2>
+            <p className=" text-white">
+              Description of the Placeholder here.
+            </p>
+          </div>
+        </div>
+        <div className="p-4 border border-gray-200 rounded-md hover:shadow-md">
+          <img
+            src="Placeholder.jpg"
+            alt="Placeholder"
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-4">
+            <h2 className="text-lg font-bold">Placeholder</h2>
+            <p className=" text-white">
+              Description of the Placeholder here.
+            </p>
+          </div>
+        </div>
+        <div className="p-4 border border-gray-200 rounded-md hover:shadow-md">
+          <img
+            src="Placeholder.jpg"
+            alt="Placeholder"
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-4">
+            <h2 className="text-lg font-bold">Placeholder</h2>
+            <p className=" text-white">
+              Description of the Placeholder here.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Speiseplan
+export default Speiseplan;

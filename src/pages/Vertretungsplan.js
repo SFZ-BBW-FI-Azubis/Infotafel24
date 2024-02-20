187c4a1f41a04ca25ebc756391db97876a6fbdaf
Justin <j.pietzsch@bbw-fi.de>
2024-02-20 07:35:36 +0100

    Vertretungsplan Updated
    Table is now generated via js instead of manually
    Added another "sub navbar" for the years
    Changed some "css" parameters to make it look better

--- src/pages/Vertretungsplan.js
+++ src/pages/Vertretungsplan.js
@@ -18,6 +18,18 @@ function Vertretungsplan() {
     "Sonstige",
   ];
 
+  const year = [
+    "erstes",
+    "zweites",
+    "drittes"
+  ];
+
+  const tableData = [
+    ["Row 1, Cell 1", "Row 1, Cell 2", "Row 1, Cell 3", "Row 1, Cell 4", "Row 1, Cell 5", "Row 1, Cell 6", "Row 1, Cell 7", "Row 1, Cell 8", "Row 1, Cell 9", "Row 1, Cell 10"],
+    ["Row 2, Cell 1", "Row 2, Cell 2", "Row 2, Cell 3", "Row 2, Cell 4", "Row 2, Cell 5", "Row 2, Cell 6", "Row 2, Cell 7", "Row 2, Cell 8", "Row 2, Cell 9", "Row 2, Cell 10"],
+    ["Row 3, Cell 1", "Row 3, Cell 2", "Row 3, Cell 3", "Row 3, Cell 4", "Row 3, Cell 5", "Row 3, Cell 6", "Row 3, Cell 7", "Row 3, Cell 8", "Row 3, Cell 9", "Row 3, Cell 10"],
+  ];
+
   useEffect(() => {
     // Disable scrolling
     document.body.style.overflow = "hidden";
@@ -56,45 +68,34 @@ function Vertretungsplan() {
           ))}
         </div>
       </div>
-      <div className="table-container flex-grow">
-        <table className="mx-auto h-full">
-          <tbody className="min-h-screen border-white border-2 text-3xl font-thin">
-            <tr className="border-white border-2">
-              <td className="text-white bordered">Row 1, Cell 1</td>
-              <td className="text-white bordered">Row 1, Cell 2</td>
-              <td className="text-white bordered">Row 1, Cell 3</td>
-              <td className="text-white bordered">Row 1, Cell 4</td>
-              <td className="text-white bordered">Row 1, Cell 5</td>
-              <td className="text-white bordered">Row 1, Cell 6</td>
-              <td className="text-white bordered">Row 1, Cell 7</td>
-              <td className="text-white bordered">Row 1, Cell 8</td>
-              <td className="text-white bordered">Row 1, Cell 9</td>
-              <td className="text-white bordered">Row 1, Cell 10</td>
-            </tr>
-            <tr className="border-white border-2">
-              <td className="text-white bordered">Row 2, Cell 1</td>
-              <td className="text-white bordered">Row 2, Cell 2</td>
-              <td className="text-white bordered">Row 2, Cell 3</td>
-              <td className="text-white bordered">Row 2, Cell 4</td>
-              <td className="text-white bordered">Row 2, Cell 5</td>
-              <td className="text-white bordered">Row 2, Cell 6</td>
-              <td className="text-white bordered">Row 2, Cell 7</td>
-              <td className="text-white bordered">Row 2, Cell 8</td>
-              <td className="text-white bordered">Row 2, Cell 9</td>
-              <td className="text-white bordered">Row 2, Cell 10</td>
-            </tr>
-            <tr className="border-white border-2">
-              <td className="text-white bordered">Row 3, Cell 1</td>
-              <td className="text-white bordered">Row 3, Cell 2</td>
-              <td className="text-white bordered">Row 3, Cell 3</td>
-              <td className="text-white bordered">Row 3, Cell 4</td>
-              <td className="text-white bordered">Row 3, Cell 5</td>
-              <td className="text-white bordered">Row 3, Cell 6</td>
-              <td className="text-white bordered">Row 3, Cell 7</td>
-              <td className="text-white bordered">Row 3, Cell 8</td>
-              <td className="text-white bordered">Row 3, Cell 9</td>
-              <td className="text-white bordered">Row 3, Cell 10</td>
-            </tr>
+      <div className="sub-navbar flex justify-center">
+        <div className="flex text-white border-b-2 border-b-white rounded-sm text-3xl font-thin pt-10 pb-2 flex-wrap">
+          {year.map((item, index) => (
+            <button
+              key={index}
+              className={`px-4 py-2 ml-10 uppercase ${
+                selectedItem === item ? "selected font-normal" : ""
+              }`}
+              onClick={() => handleItemClick(item)}
+              style={{ cursor: "pointer" }}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="table-container flex-grow flex justify-center items-center">
+        <table className="mx-auto">
+          <tbody className="border-white border-2 text-3xl font-thin">
+            {tableData.map((row, rowIndex) => (
+              <tr key={rowIndex} className="border-white bordered">
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex} className="text-white bordered" style={{ border: "1px solid white", height: "150px", width: "200px" }}>
+                    {cell}
+                  </td>
+                ))}
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
